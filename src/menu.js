20bb13415be9368edc906556f31615e25df32bb8
Julian <96155980+wulfiss@users.noreply.github.com>
2022-12-06 19:10:21 -0300

    Show img in menu list

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,6 @@
 import food from "./menulist";
 
 const menu = () => {
-    function importAll(r){
-        return r.keys().map(r);
-    }
-
-    const images = importAll(require.context('./src/img', false, /\.(png|svg|jpg|jpeg|gif)$/));
-    console.log(images);
-
     const content_container = document.createElement('div');
     
     content_container.classList.add('menu');
@@ -16,7 +9,8 @@ const menu = () => {
     menu_div.textContent = food[0]['name'];
 
     const food_img = document.createElement('img');
-    food_img.setAttribute('src', images[`${food[1]['img']}`]);
+    food_img.setAttribute('src', `./img/${food[0]['img']}`);
+    food_img.classList.add('menuImg');
 
     content_container.appendChild(food_img);
     content_container.appendChild(menu_div);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[name][ext]',
     },
     mode: 'development',
     devtool: 'inline-source-map',
@@ -24,4 +26,11 @@ module.exports = {
            },
         ],
     },
+    plugins:[
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/img', to: 'img'},
+            ]
+        })
+    ]
 };
