2193bc5953389148bf53c09f7d2b4cb8aa4f7dea
Julian <96155980+wulfiss@users.noreply.github.com>
2022-12-06 19:19:50 -0300

    Add forloop show showing menu

--- src/menu.js
+++ src/menu.js
@@ -4,16 +4,20 @@ const menu = () => {
     const content_container = document.createElement('div');
     
     content_container.classList.add('menu');
+    
+    for(let i = 0; i < food.length; i++){
 
-    const menu_div = document.createElement('div');
-    menu_div.textContent = food[0]['name'];
+        const menu_div = document.createElement('div');
+        menu_div.textContent = food[i]['name'];
 
-    const food_img = document.createElement('img');
-    food_img.setAttribute('src', `./img/${food[0]['img']}`);
-    food_img.classList.add('menuImg');
+        const food_img = document.createElement('img');
+        food_img.setAttribute('src', `./img/${food[i]['img']}`);
+        food_img.classList.add('menuImg');
 
-    content_container.appendChild(food_img);
-    content_container.appendChild(menu_div);
+        menu_div.appendChild(food_img);
+    
+        content_container.appendChild(menu_div);
+    }
 
     return content_container;
 }
