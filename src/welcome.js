15677e3a3f04d891e8c6a93ef35f78edcfdaacc2
Julian <96155980+wulfiss@users.noreply.github.com>
2022-12-13 19:38:11 -0300

    add more styling

--- src/welcome.js
+++ src/welcome.js
@@ -2,9 +2,18 @@ const welcome = () => {
     const content_container = document.createElement('div');
     content_container.classList.add('welcome');
 
-    const welcome_div = document.createElement('div');
-    welcome_div.textContent = 'Welcome to the page of the Best Restaurant of the WORLD!!';
+    const portal_img = document.createElement('img');
+    portal_img.setAttribute('src', './img/welcome.jpg');
+    portal_img.setAttribute('alt', 'Two people cooking')
+    portal_img.classList.add('welcomeImg');
 
+
+    const welcome_div = document.createElement('div');
+    const p_welcome_mgs = document.createElement('p');
+    p_welcome_mgs.textContent ='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac libero semper, volutpat tortor id, aliquet lacus. Praesent elementum convallis lorem id interdum. Praesent vitae purus augue. Fusce efficitur odio sit amet est cursus, quis finibus dolor malesuada. Pellentesque id eros rhoncus ipsum laoreet dapibus sit amet sit amet neque.';
+    welcome_div.appendChild(p_welcome_mgs);
+    
+    content_container.appendChild(portal_img);
     content_container.appendChild(welcome_div);
 
     return content_container;
