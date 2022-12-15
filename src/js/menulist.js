03cdd09bc9ac9bbc36f3f3cec1701bda97ec28a1
Julian <96155980+wulfiss@users.noreply.github.com>
2022-12-15 20:27:11 -0300

    fix center img in menu list

--- src/js/menulist.js
+++ src/js/menulist.js
@@ -1,7 +1,7 @@
 
 let food = [
-    {name: 'Food1', ingredients: 'ing1, ing2', price: '123.32', img: 'foodone.jpg'},
-    {name: 'Food2', ingredients: 'ing3, ing4', price: '234.54', img: 'foodtwo.jpg'},
+    {name: 'Food1', ingredients: 'ing1, ing2 ing1, ing2ing1, ing2 ', price: '123.32', img: 'foodone.jpg'},
+    {name: 'Food2', ingredients: 'ing1, ing2 ing1, ing2ing1, ing2 ', price: '234.54', img: 'foodtwo.jpg'},
     {name: 'Food3', ingredients: 'ing5, ing6', price: '231.23', img: 'foodthree.jpg'},
     {name: 'Food4', ingredients: 'ing7, ing8', price: '231.23', img: 'foodthree.jpg'},
     {name: 'Food5', ingredients: 'ing5, ing6', price: '231.23', img: 'foodthree.jpg'},
